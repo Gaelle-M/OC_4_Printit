902eb3d8e9f88e91657f6db805f5b581a7e0a07d
Gaelle-M <gaelle-mazzali@hotmail.fr>
2024-11-29 11:38:17 +0100

    Ajout des bullet points et difference avec le point actif

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 const bannerImg = document.querySelector('.banner-img');
 const arrowLeft = document.querySelector('.arrow_left');
 const arrowRight = document.querySelector('.arrow_right');
+const dotsContainer = document.querySelector('.dots');
 
 arrowLeft.addEventListener("click", () => {
     console.log("Vous avez cliqué sur la flèche gauche")
@@ -27,3 +28,35 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let currentIndex = 0;
+
+function bulletActive() {
+    // Mise à jour de l'image
+    const currentSlide = slides[currentIndex];
+    bannerImg.src = `./assets/images/slideshow/${currentSlide.image}`;
+    bannerImg.alt = currentSlide.tagLine;
+
+    // Mise à jour des points
+    const dots = document.querySelectorAll('.dot');
+    dots.forEach((dot, index) => {
+        dot.classList.remove('dot_selected');  // Retirer la classe de tous les points
+        if (index === currentIndex) {
+            dot.classList.add('dot_selected');  // Ajouter la classe au point actif
+        }
+    });
+}
+
+for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement('div');
+    dot.classList.add('dot');
+    
+    dot.addEventListener('click', () => {
+        currentIndex = i;
+        bulletActive();
+    });
+
+    dotsContainer.appendChild(dot);
+}
+
+bulletActive();
