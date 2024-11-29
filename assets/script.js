7d551a799116b34355907a8a0ebc63442f54584f
Gaelle-M <gaelle-mazzali@hotmail.fr>
2024-11-29 11:52:18 +0100

    Modification du javascript afin de créer un défilement infini du slider

--- assets/script.js
+++ assets/script.js
@@ -3,13 +3,6 @@ const arrowLeft = document.querySelector('.arrow_left');
 const arrowRight = document.querySelector('.arrow_right');
 const dotsContainer = document.querySelector('.dots');
 
-arrowLeft.addEventListener("click", () => {
-    console.log("Vous avez cliqué sur la flèche gauche")
-});
-arrowRight.addEventListener("click", () => {
-    console.log("Vous avez cliqué sur la flèche droite")
-});
-
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -32,17 +25,15 @@ const slides = [
 let currentIndex = 0;
 
 function bulletActive() {
-    // Mise à jour de l'image
     const currentSlide = slides[currentIndex];
     bannerImg.src = `./assets/images/slideshow/${currentSlide.image}`;
     bannerImg.alt = currentSlide.tagLine;
 
-    // Mise à jour des points
     const dots = document.querySelectorAll('.dot');
     dots.forEach((dot, index) => {
-        dot.classList.remove('dot_selected');  // Retirer la classe de tous les points
+        dot.classList.remove('dot_selected');
         if (index === currentIndex) {
-            dot.classList.add('dot_selected');  // Ajouter la classe au point actif
+            dot.classList.add('dot_selected');
         }
     });
 }
@@ -58,5 +49,26 @@ for (let i = 0; i < slides.length; i++) {
 
     dotsContainer.appendChild(dot);
 }
+
+function goToNextSlide() {
+    if (currentIndex < slides.length - 1) {
+        currentIndex++;
+    } else {
+        currentIndex = 0; 
+    }
+    bulletActive();
+}
+
+function goToPrevSlide() {
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = slides.length - 1;
+    }
+    bulletActive();
+}
+
+arrowLeft.addEventListener("click", goToPrevSlide);
+arrowRight.addEventListener("click", goToNextSlide);
 
 bulletActive();
